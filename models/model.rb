770b9cd5d776010d63f5356dae5ce640cfca1a5c
Aatish <aatish17varma@gmail.com>
2015-07-16 14:07:15 +0000

    eightball class

---
+++ models/model.rb
@@ -0,0 +1,27 @@
+class eightball
+  attr_accessor :array
+  
+  def initialize(*array)
+    array.each do |x|
+      @array.push(x)
+    end
+  end
+ 
+  def add(element)
+    @array.push(element)
+  end 
+ 
+  def print()
+    @array.each do |element|
+      push "Your element is:"    
+    end
+  end
+  
+  
+  def rselect
+    len = @array.length
+    index = Random.rand(1, len)
+    return @array[index - 1]
+  end
+
+end
