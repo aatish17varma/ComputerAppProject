c8dd41372358aac6ae4becfe738275ba97538bbd
jackson987 <robbins.jackson1@gmail.com>
2015-07-17 12:55:56 +0000

    changed submission to be text based parsed by space and comma

--- application_controller.rb
+++ application_controller.rb
@@ -10,16 +10,36 @@ class MyApp < Sinatra::Base
   
   post '/results' do
 
-    puts params
-    puts params[:firstclothing]
-
-    puts params[:firstclothing[0]]
-
-    shirt = Eightball.new
+    slist = params[:firstclothing].split(', ')
+    shirts = Eightball.new
+    
+    slist.each do |x|
+      shirts.add(x)
+    end
+    
+    plist = params[:secondclothing].split(', ')
     pants = Eightball.new
-    shirt.add(params[:firstclothing])
-    pants.add(params[:secondclothing])
+    
+    plist.each do |x|
+      shirts.add(x)
+    end
+    
     @selected = [shirt.rselect, pants.rselect]
+    
+    
+    
+#     puts params
+#     puts params[:firstclothing]
+
+#     puts params[:firstclothing[0]]
+
+#     shirt = Eightball.new
+#     pants = Eightball.new
+#     shirt.add(params[:firstclothing])
+#     pants.add(params[:secondclothing])
+#     @selected = [shirt.rselect, pants.rselect]
+    
+    ...
     erb :results
   end
   
